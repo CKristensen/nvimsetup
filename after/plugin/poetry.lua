3dc817d9bb7cd810e591664b8e9b25ac7c5a985d
Carl Kristensen <carl.johan.coelho.kristensen@schibsted.com>
2022-12-21 13:17:13 +0100

    init my awesome nvim config

---
+++ after/plugin/poetry.lua
@@ -0,0 +1,3 @@
+vim.g["g:poetv_auto_activate"] = 1
+vim.g["g:poetv_set_environment"] = 1
+vim.g["g:poetv_executables"] = ('poetry')
---
+++ lua/k1rac/init.lua
@@ -0,0 +1,3 @@
+require("k1rac.packer")
+require("k1rac.remap")
+require("k1rac.set")
