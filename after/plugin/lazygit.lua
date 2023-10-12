3a830f7a9d1a01fefefa04c73016ad32277c22ba
Carl Kristensen <carl.johan.coelho.kristensen@schibsted.com>
2023-10-12 23:48:56 +0200

    lazygit plus others

---
+++ after/plugin/lazygit.lua
@@ -0,0 +1,3 @@
+-- nnoremap <silent> <leader>gg :LazyGit<CR>
+
+vim.keymap.set("n", "<leader>gs", vim.cmd.LazyGit)
--- lua/k1rac/packer.lua
+++ lua/k1rac/packer.lua
@@ -4,49 +4,58 @@
 vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function(use)
-	use{'wbthomason/packer.nvim'}
-	use ({'dracula/vim',
-		as = 'dracula',
-		config = function() 
-			vim.cmd('colorscheme dracula')
-		end})
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
-	use{'nvim-lua/plenary.nvim'}
-	use{'ThePrimeagen/harpoon'}
-	use{'mbbill/undotree'}
-	use{'tpope/vim-fugitive'}
-    use {'hrsh7th/nvim-cmp'}
-    use({'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" }})
-	use {
+    use { 'wbthomason/packer.nvim' }
+    use({
+        'dracula/vim',
+        as = 'dracula',
+        config = function()
+            vim.cmd('colorscheme dracula')
+        end
+    })
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+    use { 'nvim-lua/plenary.nvim' }
+    use { 'ThePrimeagen/harpoon' }
+    use { 'mbbill/undotree' }
+    use({
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        requires = {
+            "nvim-lua/plenary.nvim",
+        },
+    })
+    use { 'hrsh7th/nvim-cmp' }
+    use({ 'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" } })
+    use {
 
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-		}
-	}
-	use('petobens/poet-v')
-    use{'onsails/lspkind.nvim'}
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
+    use('petobens/poet-v')
+    use { 'onsails/lspkind.nvim' }
+    use { 'vimwiki/vimwiki' }
+
 end)
-
